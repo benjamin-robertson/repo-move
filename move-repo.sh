653130f8622ff2b2364af69bd7605b9be8eccd2f
Ubuntu <ubuntu@ip-172-31-42-78.ap-southeast-2.compute.internal>
2021-04-29 13:28:47 +0000

    first commit - not working

---
+++ move-repo.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+#
+# Usage:
+# See syntax of repolist
+# Include repo to move in this file. By default script will look for repolist, other specify a argment for repo to move
+# EG, ./move-repo.sh /path/to/repolist
+#
+firstarg=$1
+
+# Set repolist location
+if [[ $firstarg == "" ]]
+then
+  repolist="./repolist"
+else
+  repolist=$firstarg
+fi
+
+# Get length of repolist
+length=`wc $repolist | awk '{ print $1 }'`
+length=$(($length - 1))
+
+count=0
+repos=()
+
+for i in `tail -n $length $repolist`
+do
+  repos+=( $i )
+  count=$(($count + 1))
+done
+
+
+for ((x=0;x < $count; x=$x + 2)); do
+  x2=$(($x + 1))
+  echo "Cloning repo ${repos[$x]} to ${repos[$x2]}"
+
+  git clone ${repos[$x]}
+  # get filename
+  folder=`echo ${repos[$x]} | awk -F "/" '{ print $2 }' | sed 's/.git//g'`
+  cd $folder
+  for branch in $(git branch -r | grep -v 'origin\/HEAD' | awk '{ print $1 }' | sed 's/origin\///g'); do                                          
+    git checkout -B $branch origin/$branch                                                                                                        
+    git pull origin $branch                                                                                                                       
+  done; 
+done
